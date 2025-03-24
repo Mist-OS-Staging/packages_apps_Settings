2ba1c386547428cd2d3a001b10ee997ae7af9eaa
ShukakuZa <yusufzuzu787@gmail.com>
2025-03-24 21:39:06 +0000

    Revert "bluetooth: Introduce Savitech LHDC Codec [4/5]"
    This reverts commit 0b5efd54a7442e7a3ef253e5f22312ecaef475de.

--- src/com/android/settings/development/bluetooth/BluetoothBitPerSampleDialogPreferenceController.java
+++ src/com/android/settings/development/bluetooth/BluetoothBitPerSampleDialogPreferenceController.java
@@ -61,12 +61,8 @@ public class BluetoothBitPerSampleDialogPreferenceController extends
             case 0:
                 final BluetoothCodecConfig currentConfig = getCurrentCodecConfig();
                 if (currentConfig != null) {
-                   /* Savitech patch: pick default by native decision, not always get the Highest one */
-                   if (currentConfig.getCodecType() != BluetoothCodecConfig.SOURCE_CODEC_TYPE_LHDCV3 &&
-                       currentConfig.getCodecType() != BluetoothCodecConfig.SOURCE_CODEC_TYPE_LHDCV5) {
-                           bitsPerSampleValue = getHighestBitsPerSample(getSelectableByCodecType(
-                               currentConfig.getCodecType()));
-                   }
+                    bitsPerSampleValue = getHighestBitsPerSample(getSelectableByCodecType(
+                            currentConfig.getCodecType()));
                 }
                 break;
             case 1:
--- src/com/android/settings/development/bluetooth/BluetoothChannelModeDialogPreferenceController.java
+++ src/com/android/settings/development/bluetooth/BluetoothChannelModeDialogPreferenceController.java
@@ -61,12 +61,8 @@ public class BluetoothChannelModeDialogPreferenceController extends
             case 0:
                 final BluetoothCodecConfig currentConfig = getCurrentCodecConfig();
                 if (currentConfig != null) {
-                    /* Savitech patch: pick default by native decision, not always get the Highest one */
-                    if (currentConfig.getCodecType() != BluetoothCodecConfig.SOURCE_CODEC_TYPE_LHDCV3 &&
-                        currentConfig.getCodecType() != BluetoothCodecConfig.SOURCE_CODEC_TYPE_LHDCV5) {
-                            channelModeValue = getHighestChannelMode(getSelectableByCodecType(
-                                currentConfig.getCodecType()));
-                    }
+                    channelModeValue = getHighestChannelMode(getSelectableByCodecType(
+                            currentConfig.getCodecType()));
                 }
                 break;
             case 1:
